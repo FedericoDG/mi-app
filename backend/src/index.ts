8ddba5cf2844878ad0b4e5dbd7e8e2945539cf59
Federico González <alambratore@hotmail.com>
2026-03-22 19:56:23 -0300

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: 'postgres',
+  user: 'user',
+  password: 'password',
+  database: 'app_db',
+  port: 5432,
+});
+
+const app = express();
+app.use(cors());
+
+app.get('/api', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.json({
+    message: 'API funcionando 🚀',
+    time: result.rows[0],
+  });
+});
+
+app.listen(4000, () => {
+  console.log('Backend corriendo en puerto 4000');
+});
