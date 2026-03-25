d5dfd0a9a60257c58c9a95d06b35d2e4141f5ceb
Federico González <alambratore@hotmail.com>
2026-03-25 18:46:04 -0300

    New H1 content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Deploy Automático</h1>
+          <h1>CI implementado</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
